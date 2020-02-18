d4c047aacb2bc9289be1a370396a0990dc13f609
Blocher <blocher1997@gmail.com>
2020-02-18 15:47:20 +0100

    Updates

---
+++ precache-manifest.b493d5010e38167ca91c7523afdb1435.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3d05d3fa508a0d626592631354b2575",
+    "url": "/Fiszki-online/index.html"
+  },
+  {
+    "revision": "80a1c2b68179bc381622",
+    "url": "/Fiszki-online/static/css/main.dfbd1880.chunk.css"
+  },
+  {
+    "revision": "5692cd457c2fd3a4f698",
+    "url": "/Fiszki-online/static/js/2.9c8f7cea.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Fiszki-online/static/js/2.9c8f7cea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80a1c2b68179bc381622",
+    "url": "/Fiszki-online/static/js/main.c3cdda4e.chunk.js"
+  },
+  {
+    "revision": "9f83bac60d74219eb700",
+    "url": "/Fiszki-online/static/js/runtime-main.04860b17.js"
+  }
+]);
